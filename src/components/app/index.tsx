9e1303b95be36a09f6e03177883a9177693d73cd
Sachin Sharma <sachin2252sharma@gmail.com>
2023-06-06 00:57:44 +0530

    added app marketing modal

---
+++ src/components/app/index.tsx
@@ -0,0 +1,109 @@
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
+import { useQuery } from "../../hooks/useQuery";
+
+function AppMarketingModal(props: any) {
+  const isMobile = useQuery();
+  return (
+    <Modal
+      {...props}
+      size="lg"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+      style={{ zIndex: "9999999999999" }}
+    >
+      <Modal.Header closeButton>
+        <Modal.Title id="contained-modal-title-vcenter">
+          Agrarian Pvt Ltd
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <h4 className="text-center">We have just launched out App.</h4>
+        <div
+          className="d-flex justify-content-center align-items-center mx-4"
+          style={{ flexWrap: "wrap" }}
+        >
+          <div className={isMobile ? "" : "w-50"}>
+            <h6>
+              <span>✅</span>
+              Users who install the app now will get lifetime Premium.
+            </h6>
+            <h6>
+              <span>✅</span>Join and contact us through our wide farmer
+              community.
+            </h6>
+            <h6>
+              <span>✅</span>Not satisfied with our services? Send you feed to
+              us with integrated feedback system.
+            </h6>
+            <h6>
+              <span>✅</span>27/7 Active support available on the app.
+            </h6>
+            <div
+              className="d-flex align-items-center flex-wrap"
+              style={{ flexWrap: "wrap" }}
+            >
+              <img
+                src="/assets/app-qr.jpg"
+                alt="app-qr"
+                width={120}
+                height={120}
+              />
+              <div className="d-flex align-items-center flex-wrap flex-column gap-3">
+                <a
+                  href="https://play.google.com/store/apps/details?id=com.agrarian"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <img
+                    src="/assets/playstore.png"
+                    height={60}
+                    alt="Play Store"
+                    // className="mx-4"
+                  />
+                </a>
+                <a
+                  rel="noreferrer"
+                  href="https://www.facebook.com/profile.php?id=100088894643348"
+                  target="_blank"
+                ></a>
+              </div>
+            </div>
+          </div>
+          {!isMobile && (
+            <div className="w-50 d-flex justify-content-center align-items-center flex-column">
+              <img
+                src="/assets/app.jpg"
+                alt="app-mockup"
+                height={400}
+                width={250}
+              />
+            </div>
+          )}
+        </div>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button onClick={props.onHide}>Close</Button>
+      </Modal.Footer>
+    </Modal>
+  );
+}
+
+export default AppMarketingModal;
+
+// function App() {
+//     const [modalShow, setModalShow] = React.useState(false);
+//
+//     return (
+//         <>
+//             <Button variant="primary" onClick={() => setModalShow(true)}>
+//                 Launch vertically centered modal
+//             </Button>
+//
+//             <MyVerticallyCenteredModal
+//                 show={modalShow}
+//                 onHide={() => setModalShow(false)}
+//             />
+//         </>
+//     );
+// }
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,134 +1,181 @@
 import React from "react";
+import Confetti from "react-confetti";
 import Banner from "../../components/banner";
 import Services from "../../components/services";
 import About from "../../components/About";
+import AppMarketingModal from "../../components/app";
 
 const Home = () => {
-    document.title = 'Agrarian | The Trust Of Growth'
-    localStorage.setItem('active', 'home');
-    return (
-        <div className='container-fluid'>
-            <div className='row'>
-                <div className={'col-12 banner'}>
-                    <Banner/>
-                </div>
-                <div className='container-fluid mt-5'>
-                    <div className='container'>
-                        <div className='row solutions'>
-                            <div
-                                className={'col-md-4 sol d-flex justify-content-center align-items-center flex-column'}>
-                                <img src='/assets/forest.png' height='200px' width='200px' alt='natural farming'/>
-                                <h5 className='mt-4'>Crop Seasoning</h5>
-                            </div>
-                            <div className='col-md-4 sol d-flex justify-content-center align-items-center flex-column'>
-                                <img src='/assets/recycled.png' height='200px' width='200px' alt='natural farming'/>
-                                <h5 className='mt-4'>IOT Solutions</h5>
-                            </div>
-                            <div className='col-md-4 d-flex justify-content-center align-items-center flex-column'>
-                                <img src='/assets/growth.png' height='200px' width='200px' alt='natural farming'/>
-                                <h5 className='mt-4'>Better Crop Quality</h5>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <Services/>
-                <div className="col-12">
-                    <div className="container-fluid">
-                        <div className="container mt-5">
-                            <div className="row">
-                                <div className="container-fluid mt-5 ab">
-                                    <div className="container">
-                                        <div className="row">
-                                            <div
-                                                className="rounded-3 col-md-6 d-flex justify-content-center align-items-center">
-                                                <img
-                                                    src="/assets/farmers.jpg"
-                                                    height="90%"
-                                                    width="100%"
-                                                    alt="logo"
-                                                />
-                                            </div>
-                                            <div className="col-md-6">
-                                                <h3 className="about" style={{fontFamily: "fantasy"}}>India At
-                                                    Glance</h3>
-                                                <p className="" style={{textAlign: "justify", fontWeight: '400'}}>
-                                                    <small style={{fontSize: '30px', fontWeight: '400'}}>W</small>ith a
-                                                    population of 1.27 billion India is the world's second most populous
-                                                    country. It is the seventh largest country in the world with an area
-                                                    of 3.288 million sq km. It has a long
-                                                    coastline of over 7,500 km. India is a diverse country where over 22
-                                                    major languages and 415 dialects
-                                                    are spoken. With the highest mountain range in the world, the
-                                                    Himalayas to its North, the Thar desert
-                                                    to its West, the Gangetic delta to its East and the Deccan Plateau
-                                                    in the South, the country is home to
-                                                    vast agro-ecological diversity.<br/><br/>
-                                                    <strong>Major economic pillar:- </strong> Around 65–70%% of the
-                                                    Indian population (directly or indirectly) depends
-                                                    upon the Agriculture sector and currently, it contributes to 16–17%
-                                                    of the GDP in the Indian economy.<br/><br/>
-
-                                                    <strong>Major Crops:- </strong><br/>
-                                                    1. Area of cultivation of Rice 45.1 Million/Ha.<br/>
-                                                    2. Area of cultivation of Wheat 31.6 Million/Ha.<br/>
-                                                    3. Area of cultivation of Pulses 29 Million/Ha.<br/> <br/>
-                                                    <strong>Livelihoods and food security for many </strong>
-                                                    Concerning Indian Agriculture provides livelihood to nearly 70% of
-                                                    the workforce. It also contributes
-                                                    immensely to the food security of the nation as it is the top
-                                                    producer of many crops.
-                                                    Good quality of seeds and new technologies is not reaching the
-                                                    farmers.
-                                                    The quality of soil is degraded due to
-                                                    contamination from the overuse of pesticides and fertilizers for
-                                                    decades.
-                                                </p>
-                                            </div>
-                                            {/*{!isMobile && (*/}
-                                            {/*)}*/}
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div className="col-12">
-                    <div className="container-fluid">
-                        <div className="container">
-                            <div className="row">
-                                <div className="container-fluid">
-                                    <div className="container">
-                                        <div className="row">
-                                            <div className="col-12 text-center mt-2">
-                                                <span className='fs-1'>The Issue On The Ground</span>
-                                            </div>
-                                            <div className="col-12 text-wrap more">
-                                                <p>
-                                                    Unavailability of quality inputs:- Good quality of seeds and new
-                                                    technologies is not reaching the farmers.
-                                                    The quality of soil is degraded due to
-                                                    contamination from the overuse of pesticides and fertilizers for
-                                                    decades.
-                                                    - Not many have access to opt for mechanization, also
-                                                    poor soil quality and varying climatic conditions
-                                                    Due to unavailability of good market farmers sells their
-                                                    harvest product in low price and middle man get benifited. so need
-                                                    good market facilities in every
-                                                    district.
-                                                </p>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <About/>
-            </div>
+  const [showModal, setShowModal] = React.useState(true);
+  document.title = "Agrarian | The Trust Of Growth";
+  localStorage.setItem("active", "home");
+  return (
+    <div className="container-fluid">
+      <div className="row">
+        <div className={"col-12 banner"}>
+          <Banner />
         </div>
-    )
-}
+        <div className="container-fluid mt-5">
+          <div className="container">
+            <div className="row solutions">
+              <div
+                className={
+                  "col-md-4 sol d-flex justify-content-center align-items-center flex-column"
+                }
+              >
+                <img
+                  src="/assets/forest.png"
+                  height="200px"
+                  width="200px"
+                  alt="natural farming"
+                />
+                <h5 className="mt-4">Crop Seasoning</h5>
+              </div>
+              <div className="col-md-4 sol d-flex justify-content-center align-items-center flex-column">
+                <img
+                  src="/assets/recycled.png"
+                  height="200px"
+                  width="200px"
+                  alt="natural farming"
+                />
+                <h5 className="mt-4">IOT Solutions</h5>
+              </div>
+              <div className="col-md-4 d-flex justify-content-center align-items-center flex-column">
+                <img
+                  src="/assets/growth.png"
+                  height="200px"
+                  width="200px"
+                  alt="natural farming"
+                />
+                <h5 className="mt-4">Better Crop Quality</h5>
+              </div>
+            </div>
+          </div>
+        </div>
+        <Services />
+        <AppMarketingModal
+          show={showModal}
+          onHide={() => setShowModal(false)}
+        />
+        {showModal && (
+          <Confetti
+            width={window.innerWidth / 2}
+            height={window.innerHeight}
+            style={{ zIndex: 10000000000 }}
+          />
+        )}
+
+        <div className="col-12">
+          <div className="container-fluid">
+            <div className="container mt-5">
+              <div className="row">
+                <div className="container-fluid mt-5 ab">
+                  <div className="container">
+                    <div className="row">
+                      <div className="rounded-3 col-md-6 d-flex justify-content-center align-items-center">
+                        <img
+                          src="/assets/farmers.jpg"
+                          height="90%"
+                          width="100%"
+                          alt="logo"
+                        />
+                      </div>
+                      <div className="col-md-6">
+                        <h3 className="about" style={{ fontFamily: "fantasy" }}>
+                          India At Glance
+                        </h3>
+                        <p
+                          className=""
+                          style={{ textAlign: "justify", fontWeight: "400" }}
+                        >
+                          <small
+                            style={{ fontSize: "30px", fontWeight: "400" }}
+                          >
+                            W
+                          </small>
+                          ith a population of 1.27 billion India is the world's
+                          second most populous country. It is the seventh
+                          largest country in the world with an area of 3.288
+                          million sq km. It has a long coastline of over 7,500
+                          km. India is a diverse country where over 22 major
+                          languages and 415 dialects are spoken. With the
+                          highest mountain range in the world, the Himalayas to
+                          its North, the Thar desert to its West, the Gangetic
+                          delta to its East and the Deccan Plateau in the South,
+                          the country is home to vast agro-ecological diversity.
+                          <br />
+                          <br />
+                          <strong>Major economic pillar:- </strong> Around
+                          65–70%% of the Indian population (directly or
+                          indirectly) depends upon the Agriculture sector and
+                          currently, it contributes to 16–17% of the GDP in the
+                          Indian economy.
+                          <br />
+                          <br />
+                          <strong>Major Crops:- </strong>
+                          <br />
+                          1. Area of cultivation of Rice 45.1 Million/Ha.
+                          <br />
+                          2. Area of cultivation of Wheat 31.6 Million/Ha.
+                          <br />
+                          3. Area of cultivation of Pulses 29 Million/Ha.
+                          <br /> <br />
+                          <strong>
+                            Livelihoods and food security for many{" "}
+                          </strong>
+                          Concerning Indian Agriculture provides livelihood to
+                          nearly 70% of the workforce. It also contributes
+                          immensely to the food security of the nation as it is
+                          the top producer of many crops. Good quality of seeds
+                          and new technologies is not reaching the farmers. The
+                          quality of soil is degraded due to contamination from
+                          the overuse of pesticides and fertilizers for decades.
+                        </p>
+                      </div>
+                      {/*{!isMobile && (*/}
+                      {/*)}*/}
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="col-12">
+          <div className="container-fluid">
+            <div className="container">
+              <div className="row">
+                <div className="container-fluid">
+                  <div className="container">
+                    <div className="row">
+                      <div className="col-12 text-center mt-2">
+                        <span className="fs-1">The Issue On The Ground</span>
+                      </div>
+                      <div className="col-12 text-wrap more">
+                        <p>
+                          Unavailability of quality inputs:- Good quality of
+                          seeds and new technologies is not reaching the
+                          farmers. The quality of soil is degraded due to
+                          contamination from the overuse of pesticides and
+                          fertilizers for decades. - Not many have access to opt
+                          for mechanization, also poor soil quality and varying
+                          climatic conditions Due to unavailability of good
+                          market farmers sells their harvest product in low
+                          price and middle man get benifited. so need good
+                          market facilities in every district.
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <About />
+      </div>
+    </div>
+  );
+};
 
 export default Home;
